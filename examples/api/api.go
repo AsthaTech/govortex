2eb667c913b9ba5074663a5e95aad788e5b72fd4
shauryam <shauryam@asthatrade.com>
2025-07-02 12:32:55 +0530

    updates api example

--- examples/api/api.go
+++ examples/api/api.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	govortex "github.com/AsthaTech/govortex/v2"
 )
@@ -16,7 +17,19 @@ func main() {
 	govortex.InitializeVortexApi(applicationId, apiKey, &client)
 	ctx := context.Background()
 
-	client.Login(ctx, "clientCode", "password", "totp")
+	// Open this URL in the browser to login. The page will redirect to the callback URL configured on the API Portal.
+	// The callback URL will contain the auth_token as a query parameter.
+	url := client.GetLoginUrl()
+	fmt.Printf("URL for login: %s\n", url)
+
+	auth_token := "your_auth_token_here" // Replace with the auth_token received after login
+
+	_, err := client.ExchangeToken(ctx, auth_token)
+	if err != nil {
+		fmt.Println("Error exchanging token:", err)
+		return
+	}
+	fmt.Println("Access Token:", client.AccessToken)
 	// Access token is automatically set upon successful login call
 
 	client.Orders(ctx)    //orders need an offset and limit
